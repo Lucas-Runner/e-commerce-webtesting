5fd3f646bccd8bd1543b64d104544a47d0cf276d
Lucas <admlucasvieira@gmail.com>
2023-06-20 17:34:43 -0300

    test adds

--- tests/spree-register.spec.js
+++ tests/spree-register.spec.js
@@ -1,34 +1,6 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-/*
-
-test('Browser Context Test', async ({ browser }) => {
-    const context = await browser.newContext();
-    const page = await context.newPage();
-
-    await page.goto("https://demo.spreecommerce.org/");
-    console.log(await page.title());
-
-    await (page.locator('.pop-close-btn')).click();
-    await (page.locator('._24EHh')).click();
-
-    await (page.locator('.nav-user-account')).click();
-    await (page.locator('.sign-btn')).click();
-    await (page.locator("[id='fm-login-id']")).type("qualitytestingdummy@gmail.com");
-    await (page.locator("[id='fm-login-password']")).type("Hazmat-Suit-1234");
-    await (page.locator('.cosmos-btn.cosmos-btn-primary.cosmos-btn-large.cosmos-btn-block.login-submit')).click();
-
-    await page.waitForLoadState('networkidle');
-    const pageContent = await page.content();
-    //const isTextPresent = pageContent.includes('olá, qualitytestingdummy!');
-    expect(pageContent).toContain('olá, qualitytestingdummy!');
-
-
-});
-
-*/
-
 test('Browser Context Test', async ({ browser }) => {
 
     const randomString = Math.random().toString(36).substring(2, 10);
@@ -56,6 +28,10 @@ test('Browser Context Test', async ({ browser }) => {
 
     await newTab.locator("[id$='spree_user_password']").type(numPassword);
     await newTab.locator("[id$='spree_user_password_confirmation']").type(numPassword);
+    await newTab.locator(".btn.btn-primary.btn-block.spree-btn.mb-5").click();
+    
+
+
     
     await page.waitForTimeout(5000);
 
