2c9fe8fa6d0783cfcc8d5377535b82e763b84801
Lucas <admlucasvieira@gmail.com>
2023-06-21 00:58:13 -0300

    email creation procedure externalized as a Funtion

---
+++ helpers/registration.js
@@ -0,0 +1,29 @@
+async function executeRegistration(browser) {
+
+    const randomString = Math.random().toString(36).substring(2, 10);
+    const baseUrl = "https://putsbox.com/random/inspect";
+    const newUrl = baseUrl.replace("random", randomString);
+    const email = `${randomString}@putsbox.com`
+
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    await page.goto(newUrl);
+    console.log(email);
+
+    const newTab = await context.newPage();
+    await newTab.goto("https://demo.spreecommerce.org/");
+    await newTab.locator("#account-button").click();
+    await newTab.locator('#link-to-account a[href="/signup"]').click();
+    await newTab.locator("[id$='spree_user_email']").type(email);
+
+    const numPassword = "123456789"
+
+    await newTab.locator("[id$='spree_user_password']").type(numPassword);
+    await newTab.locator("[id$='spree_user_password_confirmation']").type(numPassword);
+    await newTab.locator(".btn.btn-primary.btn-block.spree-btn.mb-5").click();
+
+    return { email, newTab, numPassword, page };
+}
+
+module.exports = { executeRegistration };
--- tests/spree-login.spec.js
+++ tests/spree-login.spec.js
@@ -1,35 +1,9 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
+const { executeRegistration } = require('../helpers/registration');
 
 test('Browser Context Test', async ({ browser }) => {
-
-    // Random character generator and e-mail creator with PutsBox
-    const randomString = Math.random().toString(36).substring(2, 10);
-    const baseUrl = "https://putsbox.com/random/inspect";
-    const newUrl = baseUrl.replace("random", randomString);
-    const email = `${randomString}@putsbox.com`
-
-    // E-mail page execution
-    const context = await browser.newContext();
-    const page = await context.newPage();
-
-    await page.goto(newUrl);
-    console.log(email);
-
-    // New tab for website navigation / Website Registration
-    const newContext = await browser.newContext();
-    const newTab = await context.newPage();
-
-    await newTab.goto("https://demo.spreecommerce.org/");
-    await newTab.locator("#account-button").click();
-    await newTab.locator('#link-to-account a[href="/signup"]').click();
-    await newTab.locator("[id$='spree_user_email']").type(email);
-
-    const numPassword = "123456789"
-
-    await newTab.locator("[id$='spree_user_password']").type(numPassword);
-    await newTab.locator("[id$='spree_user_password_confirmation']").type(numPassword);
-    await newTab.locator(".btn.btn-primary.btn-block.spree-btn.mb-5").click();
+    const { email, newTab, numPassword, page } = await executeRegistration(browser);
 
     // Log-out Procedure / Log-in Test
 
@@ -41,7 +15,7 @@ test('Browser Context Test', async ({ browser }) => {
     await newTab.locator("[id$='spree_user_email']").type(email);
     await newTab.locator("[id$='spree_user_password']").type(numPassword);
     await newTab.locator(".btn.btn-primary.btn-block.spree-btn.mt-2").click();
-    
+        
     /*
     // Assertions
     const searchPage = 'My Account';
