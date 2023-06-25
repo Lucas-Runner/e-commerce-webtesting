1e13905dd170b8dea68ed33c7b90590538ef2c9f
Lucas <admlucasvieira@gmail.com>
2023-06-25 14:01:58 -0300

    test adds

--- helpers/registration.js
+++ helpers/registration.js
@@ -1,16 +1,19 @@
 async function executeRegistration(browser) {
 
+    // Random character generator and e-mail creator with PutsBox
     const randomString = Math.random().toString(36).substring(2, 10);
     const baseUrl = "https://putsbox.com/random/inspect";
     const newUrl = baseUrl.replace("random", randomString);
     const email = `${randomString}@putsbox.com`
 
+    // E-mail page execution
     const context = await browser.newContext();
     const page = await context.newPage();
 
     await page.goto(newUrl);
     console.log(email);
 
+    // New tab for website navigation / Website Registration
     const newTab = await context.newPage();
     await newTab.goto("https://demo.spreecommerce.org/");
     await newTab.locator("#account-button").click();
