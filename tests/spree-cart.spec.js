9cb1d1dfcaa6a62c91baa7342a4e66d5236e8a29
Lucas <admlucasvieira@gmail.com>
2023-06-25 20:08:39 -0300

    finally spree-cart test retrieves the selected item's title and the basket item's title

--- tests/spree-cart.spec.js
+++ tests/spree-cart.spec.js
@@ -17,13 +17,16 @@ await newTab.locator("#product_65").click();
 
 const productNameLocator = await newTab.locator('.mt-3.mt-md-0.text-center.text-md-left.product-details-title');
 const productName = await productNameLocator.innerText();
-
+console.log(productName);
 await newTab.locator("#add-to-cart-button").click();
 
 // View Cart
 
 await newTab.locator('.btn.btn-outline-primary.w-100.font-weight-bold.text-uppercase.product-added-modal-button').click();
 
+const cartItemLocator = await newTab.locator('.item-title');
+const cartItemName = await cartItemLocator.innerText();
+console.log(cartItemName);
 
 await page.waitForTimeout(5000);
 
