2906a405ca1dea7b6734025a5129f5aac4532aa7
Lucas <admlucasvieira@gmail.com>
2023-06-25 14:44:36 -0300

    test adds

--- tests/spree-login.spec.js
+++ tests/spree-login.spec.js
@@ -16,15 +16,15 @@ test('Browser Context Test', async ({ browser }) => {
     await newTab.locator("[id$='spree_user_password']").type(numPassword);
     await newTab.locator(".btn.btn-primary.btn-block.spree-btn.mt-2").click();
         
-    /*
+    
     // Assertions
     const searchPage = 'My Account';
     expect(await newTab.locator(".text-uppercase.spree-mb-large.spree-mt-large.spree-header").textContent()).toContain(searchPage);
     expect(await newTab.locator(".account-page-user-info-item-definition").textContent()).toContain(email);
     
     const currentURL = newTab.url();
-    //expect(currentURL).toBe("https://demo.spreecommerce.org/account");
-    */
+    expect(currentURL).toBe("https://demo.spreecommerce.org/account");
+    
 
     await page.waitForTimeout(5000);
 
