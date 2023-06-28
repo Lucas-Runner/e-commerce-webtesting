43470ee539ed4b2fef81833bc36b026c3b0161b7
Lucas <admlucasvieira@gmail.com>
2023-06-27 23:44:45 -0300

    test adds

--- tests/spree-purchase.spec.js
+++ tests/spree-purchase.spec.js
@@ -6,27 +6,56 @@ const { executeRegistration } = require('../helpers/registration');
 test('Browser Context Test', async ({ browser }) => {
     const { email, newTab, numPassword, page } = await executeRegistration(browser);
 
-// Clicking Logo to Return Home Page
+    // formating address phase
 
-await newTab.goto("https://demo.spreecommerce.org/");
+    // Adding Adress and billing info
 
-// Clicking Shop Now, First Product and Add to Cart
+    const adressTitle = newTab.locator("[id='adress_label']");
+    const firstName = newTab.locator("[id='address_firstname']");
+    const lastName = newTab.locator("[id='address_lastname']");
+    const adress = newTab.locator("[id='address_address1']");
+    const adressContd = newTab.locator("[id='address_address2']");
+    const city = newTab.locator("[id='address_city']");
+    const state = newTab.locator("[id='address_state_id']");
 
-await newTab.locator('.btn.btn-primary.spree-btn.px-5').click();
-await newTab.locator("#product_65").click();
-await newTab.locator("#add-to-cart-button").click();
+    await newTab.locator('a[data-method="get"][href="/addresses/new"]').click();
 
-// View Cart
+    const dropboxField = await newTab.locator("[id='address_state_id']");
+    await dropboxField.click();
 
-await newTab.locator('.btn.btn-outline-primary.w-100.font-weight-bold.text-uppercase.product-added-modal-button').click();
+    // Wait for the dropdown options to be visible
+    await newTab.waitForSelector('.select2-results__option');
 
-// Performing Checkout and Purchase procedure
+    const desiredStateValue = '483'; // Replace with the desired state value
 
-await newTab.locator("[id='checkout-link']").click();
+    // Select the desired state option by its value
+    const stateOption = await newTab.locator('.select2-results__option[value="' + desiredStateValue + '"]').first();
+    await stateOption.click();
 
-// Purchase Confirmation and Assertions
+    // end of address phase
 
-await page.waitForTimeout(5000);
+
+    // Clicking Logo to Return Home Page
+
+    await newTab.goto("https://demo.spreecommerce.org/");
+
+    // Clicking Shop Now, First Product and Add to Cart
+
+    await newTab.locator('.btn.btn-primary.spree-btn.px-5').click();
+    await newTab.locator("#product_65").click();
+    await newTab.locator("#add-to-cart-button").click();
+
+    // View Cart
+
+    await newTab.locator('.btn.btn-outline-primary.w-100.font-weight-bold.text-uppercase.product-added-modal-button').click();
+
+    // Performing Checkout and Purchase procedure
+
+    await newTab.locator("[id='checkout-link']").click();
+
+    // Purchase Confirmation and Assertions
+
+    await page.waitForTimeout(5000);
 
 
 });
